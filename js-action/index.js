cf82cffda43d1081ce3c698de72d0c9f764be13b
Vigneswaran R <rvsvignesh85@gmail.com>
2026-03-06 03:38:14 +0530

    Update index.js

--- js-action/index.js
+++ js-action/index.js
@@ -2,4 +2,4 @@ const core = require("@actions/core")
 
 input = core.getInput("name")
 console.log(input) //did not use the core.info()
-core.setOutput("action-output","Hello ${input}")
+core.setOutput("action-output","Hello " + input)
